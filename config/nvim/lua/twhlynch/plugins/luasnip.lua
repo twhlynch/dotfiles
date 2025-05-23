5e633f4c18bb32e20745fc41cf5fd9498e6bbcc6
Tom Lynch <twhlynch@gmail.com>
2025-05-23 23:05:14 +1000

    update tab to expand snippet

--- config/nvim/lua/twhlynch/plugins/luasnip.lua
+++ config/nvim/lua/twhlynch/plugins/luasnip.lua
@@ -4,12 +4,20 @@ return {
     build = "make install_jsregexp",
 
 	dependencies = { "rafamadriz/friendly-snippets" },
-	
+
     config = function()
         require("luasnip.loaders.from_lua").load({ paths = "~/.config/nvim/lua/twhlynch/snippets/" })
 
         local ls = require("luasnip")
-        vim.keymap.set({ "i" }, "<Tab>", function() ls.expand() end, { silent = true })
+
+		vim.keymap.set({ "i" }, "<Tab>", function()
+			local ls = require("luasnip")
+			if ls.expand_or_jumpable() then
+				ls.expand_or_jump()
+			else
+				vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<Tab>", true, false, true), "n", false)
+			end
+		end, { silent = true })
         vim.keymap.set({ "i" }, "<C-e>", function() ls.expand() end, { silent = true})
         vim.keymap.set({ "i", "s" }, "<C-J>", function() ls.jump(1) end, { silent = true })
         vim.keymap.set({ "i", "s" }, "<C-K>", function() ls.jump(-1) end, { silent = true })
