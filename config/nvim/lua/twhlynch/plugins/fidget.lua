b63e3f2046b19136654b013bf85e8aa526bd17ad
Tom Lynch <twhlynch@gmail.com>
2025-05-24 00:56:27 +1000

    more neovim

---
+++ config/nvim/lua/twhlynch/plugins/fidget.lua
@@ -0,0 +1,9 @@
+return {
+	-- "j-hui/fidget.nvim",
+	-- event = "VeryLazy",
+	-- config = function()
+	-- 	require("fidget").setup({})
+
+	-- 	vim.notify = require("fidget").notify
+	-- end,
+}
--- config/nvim/lua/twhlynch/plugins/lualine.lua
+++ config/nvim/lua/twhlynch/plugins/lualine.lua
@@ -18,7 +18,7 @@ return {
         lualine_a = { "mode" },
         lualine_b = { "branch", "diff", "diagnostics" },
         lualine_c = { "filename" },
-        lualine_x = {},
+        lualine_x = { "location" },
         lualine_y = { "filetype" },
         lualine_z = {},
       },
