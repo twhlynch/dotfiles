2a0a3ecc723ac4e9de03797f02ae4025660470d1
Tom Lynch <twhlynch@gmail.com>
2025-05-25 02:05:47 +1000

    nvim snacks

---
+++ config/nvim/lua/twhlynch/plugins/snacks.lua
@@ -0,0 +1,31 @@
+return {
+  "folke/snacks.nvim",
+  priority = 1000,
+  lazy = false,
+  ---@type snacks.Config
+  opts = {
+    -- your configuration comes here
+    -- or leave it empty to use the default settings
+    -- refer to the configuration section below
+    lazygit = { enabled = true },
+    git = { enabled = true },
+    image = { enabled = true },
+
+    bigfile = { enabled = true },
+    dashboard = { enabled = true },
+    -- explorer = { enabled = true },
+    -- indent = { enabled = true },
+    -- input = { enabled = true },
+    -- picker = { enabled = true },
+    -- notifier = { enabled = true },
+    -- quickfirollle = { enabled = true },
+    -- scope = { enabled = true },
+    scroll = { enabled = true },
+    -- statuscolumn = { enabled = true },
+    -- words = { enabled = true },
+  },
+  keys = {
+    { "<leader>bg", function() Snacks.gitbrowse() end, desc = "Git Browse", mode = { "n", "v" } },
+    { "<leader>lg", function() Snacks.lazygit() end, desc = "Lazygit" },
+  }
+}
