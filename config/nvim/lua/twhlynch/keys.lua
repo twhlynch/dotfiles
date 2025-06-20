6b7e92ae4ef2b53a256db9f307e504de379e08fa
Tom Lynch <twhlynch@gmail.com>
2025-06-20 19:09:38 +1000

    nvim h&l to open close folds

--- config/nvim/lua/twhlynch/keys.lua
+++ config/nvim/lua/twhlynch/keys.lua
@@ -18,3 +18,42 @@ keymap.set("n", "<leader>cell", "<cmd>CellularAutomaton make_it_rain<CR>")
 
 vim.keymap.set("n", "<A-j>", "ddp", { noremap = true, silent = true })
 vim.keymap.set("n", "<A-k>", "ddkkp", { noremap = true, silent = true })
+
+-- h l to open close folds from Origami (https://github.com/chrisgrieser/nvim-origami/blob/main/lua/origami/features/fold-keymaps.lua)
+
+local function normal(cmdStr)
+	vim.cmd.normal({ cmdStr, bang = true })
+end
+
+-- `h` closes folds when at the beginning of a line.
+local function h()
+	local count = vim.v.count1 -- saved as `normal` affects it
+	for _ = 1, count, 1 do
+		local col = vim.api.nvim_win_get_cursor(0)[2]
+		if col == 0 then
+			local wasFolded = pcall(normal, "zc")
+			if not wasFolded then
+				normal("h")
+			end
+		else
+			normal("h")
+		end
+	end
+end
+
+-- `l` on a folded line opens the fold.
+local function l()
+	local count = vim.v.count1 -- count needs to be saved due to `normal` affecting it
+	for _ = 1, count, 1 do
+		local isOnFold = vim.fn.foldclosed(".") > -1
+		local action = isOnFold and "zo" or "l"
+		pcall(normal, action)
+	end
+end
+
+vim.keymap.set("n", "h", function()
+	h()
+end, { desc = "Origami h" })
+vim.keymap.set("n", "l", function()
+	l()
+end, { desc = "Origami l" })
