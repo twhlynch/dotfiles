8088ab2b1432ed35a963038a784b17278836b188
Tom Lynch <twhlynch@gmail.com>
2025-07-25 13:30:42 +1000

    html template

--- config/nvim/lua/twhlynch/keys.lua
+++ config/nvim/lua/twhlynch/keys.lua
@@ -75,3 +75,26 @@ end, { desc = "Origami l" })
 
 vim.api.nvim_create_user_command('W', 'w', { nargs = 0 })
 vim.api.nvim_create_user_command('Q', 'q', { nargs = 0 })
+
+keymap.set(
+	"n",
+	"<leader>html",
+	[[
+i<!DOCTYPE html>
+<html lang="en">
+<head>
+<title> </title>
+
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="shortcut icon" href="favicon.png">
+
+<link rel="stylesheet" href="style.css">
+<script src="main.js" defer></script>
+</head>
+<body>
+
+</body>
+</html>
+<Esc>13kf>la]]
+)
