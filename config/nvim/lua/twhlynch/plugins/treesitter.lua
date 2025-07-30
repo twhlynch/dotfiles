369734eb5046ce91b214313d76ce64b7a3af563d
Tom Lynch <twhlynch@gmail.com>
2025-07-30 15:41:44 +1000

    clean up treesitter context

--- config/nvim/lua/twhlynch/plugins/treesitter.lua
+++ config/nvim/lua/twhlynch/plugins/treesitter.lua
@@ -68,7 +68,7 @@ return {
 							return true
 						end
 
-						local max_filesize = 100 * 1024 -- 100 KB
+						local max_filesize = 1 * 1024 * 1024 -- 1MB
 						local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
 						if ok and stats and stats.size > max_filesize then
 							vim.notify("File larger than 100KB treesitter disabled for performance", vim.log.levels.WARN, { title = "Treesitter" })
@@ -104,12 +104,12 @@ return {
 			require("treesitter-context").setup({
 				enable = true, -- Enable this plugin (Can be enabled/disabled later via commands)
 				multiwindow = false, -- Enable multiwindow support.
-				max_lines = 0, -- How many lines the window should span. Values <= 0 mean no limit.
+				max_lines = 10, -- How many lines the window should span. Values <= 0 mean no limit.
 				min_window_height = 0, -- Minimum editor window height to enable context. Values <= 0 mean no limit.
 				line_numbers = true,
 				multiline_threshold = 20, -- Maximum number of lines to show for a single context
 				trim_scope = "outer", -- Which context lines to discard if `max_lines` is exceeded. Choices: 'inner', 'outer'
-				mode = "cursor", -- Line used to calculate context. Choices: 'cursor', 'topline'
+				mode = "topline", -- Line used to calculate context. Choices: 'cursor', 'topline'
 				-- Separator between context and content. Should be a single character string, like '-'.
 				-- When separator is set, the context will only show up when there are at least 2 lines above cursorline.
 				separator = nil,
