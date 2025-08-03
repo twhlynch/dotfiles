85906538cdf263f06357dca537f85fa1c6586b10
Tom Lynch <twhlynch@gmail.com>
2025-08-04 00:00:02 +1000

    clean up peek

--- config/nvim/lua/twhlynch/plugins/peek.lua
+++ config/nvim/lua/twhlynch/plugins/peek.lua
@@ -4,9 +4,7 @@ return {
 	build = "deno task --quiet build:fast",
 	config = function()
 		require("peek").setup()
-		vim.api.nvim_create_user_command("PeekOpen", require("peek").open, {})
-		vim.api.nvim_create_user_command("PeekClose", require("peek").close, {})
-		vim.keymap.set("n", "<leader>po", "<cmd>PeekOpen<CR>", { desc = "Open Peek preview", noremap = true })
-		vim.keymap.set("n", "<leader>pc", "<cmd>PeekClose<CR>", { desc = "Close Peek preview", noremap = true })
+		vim.keymap.set("n", "<leader>po", require("peek").open, { desc = "Open Peek preview", noremap = true })
+		vim.keymap.set("n", "<leader>pc", require("peek").close, { desc = "Close Peek preview", noremap = true })
 	end,
 }
