ae18a21fddaaf204c4811db7de507d117d4345cc
Tom Lynch <twhlynch@gmail.com>
2025-08-04 00:29:57 +1000

    remove unused plugins

--- config/nvim/lua/twhlynch/plugins/diffview.lua
+++
@@ -1,9 +0,0 @@
-return {
-	"sindrets/diffview.nvim",
-	dependencies = { "nvim-web-devicons" },
-	config = function()
-		local diffview = require("diffview")
-		vim.keymap.set("n", "<leader>pg", diffview.open, { desc = "Open Diffview" })
-		vim.keymap.set("n", "<leader>pG", diffview.close, { desc = "Close Diffview" })
-	end,
-}
--- config/nvim/lua/twhlynch/plugins/trouble.lua
+++
@@ -1,7 +0,0 @@
-return {
-	"folke/trouble.nvim",
-	dependencies = { "nvim-tree/nvim-web-devicons", "folke/todo-comments.nvim" },
-	config = function()
-		require("trouble").setup({})
-	end,
-}
