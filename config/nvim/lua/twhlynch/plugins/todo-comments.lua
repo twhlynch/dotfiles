b69c0fc60dc209192d18181a9a5bae4503c50075
Tom Lynch <twhlynch@gmail.com>
2025-08-21 00:34:04 +1000

    support dxrcy's non-conventional TODO comment syntax
    lol

--- config/nvim/lua/twhlynch/plugins/todo-comments.lua
+++ config/nvim/lua/twhlynch/plugins/todo-comments.lua
@@ -4,6 +4,13 @@ return {
 	dependencies = { "nvim-lua/plenary.nvim" },
 	config = function()
 		local todo_comments = require("todo-comments")
-		todo_comments.setup()
+		todo_comments.setup({
+			highlight = {
+				pattern = [[.*<((KEYWORDS).*)\s*:]],
+			},
+			search = {
+				pattern = [[\b(KEYWORDS)(\(.*\))?:]]
+			}
+		})
 	end,
 }
