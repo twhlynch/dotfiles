e34076c670ca31dc299b0c4ff8439a78966e638a
Tom Lynch <twhlynch@gmail.com>
2025-08-29 23:20:35 +1000

    keep fg selection highlight

--- config/nvim/lua/twhlynch/plugins/theme.lua
+++ config/nvim/lua/twhlynch/plugins/theme.lua
@@ -3,7 +3,12 @@ return {
 	config = function()
 		-- remove treesitter context changes
 		require("vague.groups.treesitter-context").get_colors = function(_) end
-		require("vague").setup({ transparent = true })
+		require("vague").setup({
+			transparent = true,
+			on_highlights = function(highlights, _)
+				highlights.Visual.fg = nil
+			end,
+		})
 		vim.cmd("colorscheme vague")
 		vim.cmd(":hi statusline guibg=NONE")
 	end,
