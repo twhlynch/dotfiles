fca1dcf8fd2d62501f6c9a72c134a9a0d0cdcac2
Tom Lynch <twhlynch@gmail.com>
2025-09-10 16:24:39 +1000

    add theme colors comment

--- config/nvim/lua/twhlynch/plugins/theme.lua
+++ config/nvim/lua/twhlynch/plugins/theme.lua
@@ -17,3 +17,27 @@ return {
 		vim.cmd(":hi statusline guibg=NONE")
 	end,
 }
+
+--   bg = "#141415",
+--   builtin = "#b4d4cf",
+--   comment = "#606079",
+--   constant = "#aeaed1",
+--   delta = "#f3be7c",
+--   error = "#d8647e",
+--   fg = "#cdcdcd",
+--   floatBorder = "#878787",
+--   func = "#c48282",
+--   hint = "#7e98e8",
+--   inactiveBg = "#1c1c24",
+--   keyword = "#6e94b2",
+--   line = "#252530",
+--   number = "#e0a363",
+--   operator = "#90a0b5",
+--   parameter = "#bb9dbd",
+--   plus = "#7fa563",
+--   property = "#c3c3d5",
+--   search = "#405065",
+--   string = "#e8b589",
+--   type = "#9bb4bc",
+--   visual = "#333738",
+--   warning = "#f3be7c"
