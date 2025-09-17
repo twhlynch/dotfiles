5e86ae5fe31a8bf05ca7364348a71fff209f0c8c
Tom Lynch <twhlynch@gmail.com>
2025-09-17 16:13:01 +1000

    clean up installs

--- INSTALL.sh
+++ INSTALL.sh
@@ -9,8 +9,16 @@ brew install --cask font-hack-nerd-font
 brew install --cask font-jetbrains-mono-nerd-font
 brew install --cask font-meslo-lg-nerd-font
 
-# installs
-brew install ffmpeg git git-lfs neovim tmux zoxide fzf bat eza lazygit gh ninja btop clang-format zig oh-my-posh ripgrep deno doxygen docker pnpm maven cargo stylua shfmt patchelf automake autoconf-archive gcc ncurses clang-tidy git-filter-repo neofetch tldr thefuck opam rlwrap coreutils terminal-notifier fastfetch delta
+# essential installs
+brew install git git-lfs ffmpeg docker coreutils
+# config reqs
+brew install neovim tmux zoxide fzf bat eza lazygit gh btop ripgrep oh-my-posh deno neofetch tldr thefuck fastfetch delta terminal-notifier
+# dev stuff
+brew install zig doxygen maven cargo stylua shfmt patchelf automake autoconf-archive ncurses git-filter-repo opam
+# c/cpp
+brew install cmake ninja clang-format gccclang-tidy
+# other installs
+brew install rlwrap
 
 # python
 brew install python@3.9 python@3.11 python@3.10 python@3.12 python@3.13
@@ -28,7 +36,7 @@ brew install --cask ghostty
 brew install --cask raycast
 
 # node
-brew install node
+brew install node npm pnpm
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.1/install.sh | bash
 nvm install 16
 nvm install 18
