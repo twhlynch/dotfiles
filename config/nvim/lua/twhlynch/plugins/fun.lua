ae6f2338105c17a6c3fec42b50a47e27c3687210
Tom Lynch <twhlynch@gmail.com>
2025-09-23 01:07:18 +1000

    comment out fish :'(
    for now

--- config/nvim/lua/twhlynch/plugins/fun.lua
+++ config/nvim/lua/twhlynch/plugins/fun.lua
@@ -5,31 +5,31 @@ return {
 			vim.keymap.set({ "n" }, "<leader>cell", "<cmd>CellularAutomaton make_it_rain<CR>", { noremap = true, silent = true, desc = "Make it rain" })
 		end,
 	},
-	{
-		-- "nullromo/fishtank.nvim",
-		"twhlynch/fishtank.nvim", -- until my pr is merged
-		opts = {
-			sprite = {
-				right = ">< °>",
-				left = "<° ><",
-				-- -- fish collection --
-				--    ><^, ⋗  ⋖ ⹁^><
-				--     >< ^>  <^ ><
-				--      >< ⋗  ⋖ ><
-				--       ><>  <><
-				--     >< *>  <* ><
-				--     >< °>  <° ><
-				--  ><((((•>  <•))))><
-				--   ><(((*>  <*)))><
-			},
-			screensaver = {
-				enabled = true,
-				timeout = 1000 * 60, -- 1 minute
-			},
-		},
-		config = function(_, opts)
-			local fishtank = require("fishtank")
-			fishtank.setup(opts)
-		end,
-	},
+	-- {
+	-- 	-- "nullromo/fishtank.nvim",
+	-- 	"twhlynch/fishtank.nvim", -- until my pr is merged
+	-- 	opts = {
+	-- 		sprite = {
+	-- 			right = ">< °>",
+	-- 			left = "<° ><",
+	-- 			-- -- fish collection --
+	-- 			--    ><^, ⋗  ⋖ ⹁^><
+	-- 			--     >< ^>  <^ ><
+	-- 			--      >< ⋗  ⋖ ><
+	-- 			--       ><>  <><
+	-- 			--     >< *>  <* ><
+	-- 			--     >< °>  <° ><
+	-- 			--  ><((((•>  <•))))><
+	-- 			--   ><(((*>  <*)))><
+	-- 		},
+	-- 		screensaver = {
+	-- 			enabled = true,
+	-- 			timeout = 1000 * 60, -- 1 minute
+	-- 		},
+	-- 	},
+	-- 	config = function(_, opts)
+	-- 		local fishtank = require("fishtank")
+	-- 		fishtank.setup(opts)
+	-- 	end,
+	-- },
 }
