f9753d8dff3a7d055136c858262851404d17bdc6
Tom Lynch <twhlynch@gmail.com>
2025-09-29 11:56:14 +1000

    no scrollof in non editable buffers

--- config/nvim/lua/twhlynch/autocmds.lua
+++ config/nvim/lua/twhlynch/autocmds.lua
@@ -6,3 +6,15 @@ vim.api.nvim_create_autocmd({ "TextYankPost" }, {
 		vim.hl.on_yank()
 	end,
 })
+
+-- no scrolloff if buffer readonly
+vim.api.nvim_create_autocmd({ "BufWinEnter", "WinEnter" }, {
+	group = vim.api.nvim_create_augroup("SetScrollOff", { clear = true }),
+	callback = function()
+		if vim.bo.buftype == "nowrite" or vim.bo.buftype == "prompt" or not vim.bo.modifiable or vim.bo.readonly then
+			vim.opt_local.scrolloff = 0
+		else
+			vim.opt_local.scrolloff = vim.g.user_scrolloff or 8
+		end
+	end,
+})
