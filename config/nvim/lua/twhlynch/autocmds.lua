1ba09503080aa60562d466d5d62f30071f0ffa2e
Tom Lynch <twhlynch@gmail.com>
2025-09-29 11:55:58 +1000

    clean up autocmd

--- config/nvim/lua/twhlynch/autocmds.lua
+++ config/nvim/lua/twhlynch/autocmds.lua
@@ -1,7 +1,7 @@
--- Highlight when yanking text
-vim.api.nvim_create_autocmd("TextYankPost", {
+-- highlight when yanking text
+vim.api.nvim_create_autocmd({ "TextYankPost" }, {
 	desc = "Highlight when yanking (copying) text",
-	group = vim.api.nvim_create_augroup("highlight-yank", { clear = true }),
+	group = vim.api.nvim_create_augroup("HighlightOnYank", { clear = true }),
 	callback = function()
 		vim.hl.on_yank()
 	end,
