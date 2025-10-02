494b65beb749947145fa6abc2dd80ec14a19a743
Tom Lynch <twhlynch@gmail.com>
2025-10-02 19:52:40 +1000

    lsp fixes

--- config/nvim/lua/twhlynch/plugins/lsp.lua
+++ config/nvim/lua/twhlynch/plugins/lsp.lua
@@ -33,7 +33,7 @@ local lsp_list = {
 	"vtsls",
 	"vue_ls",
 	"astro",
-	"mdx-analyzer",
+	"mdx_analyzer",
 	-- configs
 	"yamlls",
 }
@@ -110,22 +110,6 @@ return {
 		vim.lsp.enable(lsp_list)
 		vim.lsp.enable("swipl")
 
-		local l = vim.lsp
-		l.handlers["textDocument/hover"] = function(_, result, ctx, config)
-			config = config or { border = "rounded", focusable = true }
-			config.focus_id = ctx.method
-			if not (result and result.contents) then
-				return
-			end
-			local markdown_lines = l.util.convert_input_to_markdown_lines(result.contents)
-			markdown_lines = vim.tbl_filter(function(line)
-				return line ~= ""
-			end, markdown_lines)
-			if vim.tbl_isempty(markdown_lines) then
-				return
-			end
-			return l.util.open_floating_preview(markdown_lines, "markdown", config)
-		end
 
 		local autocmd = vim.api.nvim_create_autocmd
 		autocmd({ "BufEnter", "BufWinEnter" }, {
