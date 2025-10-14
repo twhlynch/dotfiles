0e61a7bce3ac9cad97e56f6d1c32cd649462776f
Tom Lynch <twhlynch@gmail.com>
2025-10-14 22:05:16 +1100

    change keymapping function and add [p paste above/below

--- config/nvim/lua/twhlynch/keys.lua
+++ config/nvim/lua/twhlynch/keys.lua
@@ -2,8 +2,8 @@ vim.g.mapleader = " "
 
 local set = vim.keymap.set
 
-local function desc(description)
-	return { noremap = true, silent = true, desc = description }
+local function map(modes, lhs, rhs, desc)
+	vim.keymap.set(modes, lhs, rhs, { desc = desc })
 end
 
 -- mistypes
@@ -14,51 +14,54 @@ vim.cmd([[cabbrev WQ wq]])
 vim.cmd([[cabbrev wQ wq]])
 
 -- useful
-set({ "n", "v", "x" }, "<leader>y", '"+y', desc("Yank to system clipboard"))
-set({ "n", "v", "x" }, "<A-p>", '"0p', desc("Paste last yanked register"))
-set({ "n", "v", "x" }, "<leader>w", "<C-w>", desc("Window commands"))
-set({ "n" }, "<leader>A", "ggVG", desc("Select all"))
-set({ "x" }, "/", "<esc>/\\%V", desc("Search in selection"))
+map({ "n", "v", "x" }, "<leader>y", '"+y', "Yank to system clipboard")
+map({ "n", "v", "x" }, "<A-p>", '"0p', "Paste last yanked register")
+map({ "n", "v", "x" }, "<leader>w", "<C-w>", "Window commands")
+map({ "n" }, "<leader>A", "ggVG", "Select all")
+map({ "x" }, "/", "<esc>/\\%V", "Search in selection")
 -- big move
-set({ "n" }, "H", "^")
-set({ "n" }, "L", "$")
+map({ "n" }, "H", "^")
+map({ "n" }, "L", "$")
+-- paste above or below
+map({ "n" }, "[p", '<Cmd>exe "put! " . v:register<CR>', "Paste Above")
+map({ "n" }, "]p", '<Cmd>exe "put "  . v:register<CR>', "Paste Below")
 -- moving lines
-set("i", "<A-j>", "<Esc>:m .+1<cr>==gi", desc("Move line down"))
-set("i", "<A-k>", "<Esc>:m .-2<cr>==gi", desc("Move line up"))
-set({ "n" }, "<A-j>", ":m .+1<cr>==", desc("Move line down"))
-set({ "n" }, "<A-k>", ":m .-2<cr>==", desc("Move line up"))
-set({ "x" }, "<A-j>", ":m '>+1<CR>gv=gv", desc("Move lines down"))
-set({ "x" }, "<A-k>", ":m '<-2<CR>gv=gv", desc("Move lines up"))
+map({ "i" }, "<A-j>", "<Esc>:m .+1<cr>==gi", "Move line down")
+map({ "i" }, "<A-k>", "<Esc>:m .-2<cr>==gi", "Move line up")
+map({ "n" }, "<A-j>", ":m .+1<cr>==", "Move line down")
+map({ "n" }, "<A-k>", ":m .-2<cr>==", "Move line up")
+map({ "x" }, "<A-j>", ":m '>+1<CR>gv=gv", "Move lines down")
+map({ "x" }, "<A-k>", ":m '<-2<CR>gv=gv", "Move lines up")
 -- center search
-set({ "n", "v" }, "<A-n>", "nzz", desc("Center next match"))
-set({ "n", "v" }, "<A-N>", "Nzz", desc("Center previous match"))
+map({ "n", "v" }, "<A-n>", "nzz", "Center next match")
+map({ "n", "v" }, "<A-N>", "Nzz", "Center previous match")
 -- indenting
-set({ "v" }, "<", "<gv", desc("Unindent and stay in visual"))
-set({ "v" }, ">", ">gv", desc("Indent and stay in visual"))
+map({ "v" }, "<", "<gv", "Unindent and stay in visual")
+map({ "v" }, ">", ">gv", "Indent and stay in visual")
 -- jumping
-set({ "n" }, "<leader>jo", "<cmd>b#<CR>", desc("Jump to last buffer"))
+map({ "n" }, "<leader>jo", "<cmd>b#<CR>", "Jump to last buffer")
 -- toggles
-set({ "n" }, "<leader>z", function()
+map({ "n" }, "<leader>z", function()
 	vim.wo.wrap = not vim.wo.wrap
 	print("Line wrapping is " .. (vim.wo.wrap and "ON" or "OFF"))
-end, desc("Toggle wrapping"))
-set({ "n" }, "<leader>hl", function()
+end, "Toggle wrapping")
+map({ "n" }, "<leader>hl", function()
 	vim.o.hlsearch = not vim.o.hlsearch
 	print("Search highlighting is " .. (vim.o.hlsearch and "ON" or "OFF"))
-end, desc("Toggle search highlight"))
+end, "Toggle search highlight")
 -- next prevs
-set({ "n" }, "]e", function()
+map({ "n" }, "]e", function()
 	vim.diagnostic.goto_next({
 		severity = vim.diagnostic.severity.ERROR,
 	})
-end, desc("Jump to the next error in the current buffer"))
-set({ "n" }, "[e", function()
+end, "Jump to the next error in the current buffer")
+map({ "n" }, "[e", function()
 	vim.diagnostic.goto_prev({
 		severity = vim.diagnostic.severity.ERROR,
 	})
-end, desc("Jump to the previous error in the current buffer"))
+end, "Jump to the previous error in the current buffer")
 -- macros
-set({ "n" }, "<leader>mm", "<cmd>%s/\\r//<cr>", desc("Remove trailing ^M"))
-set({ "n" }, "<leader>mx", ":!chmod +x %<CR><CR>", desc("chmod +x"))
-set({ "n" }, "<leader>mc", "yygccp", desc("Duplicate and comment out line"))
-set({ "x" }, "<leader>mr", ":<esc>:%s/\\%V[^A-Za-z0-9]/ /g<CR>", desc("Clean selection for type"))
+map({ "n" }, "<leader>mm", "<cmd>%s/\\r//<cr>", "Remove trailing ^M")
+map({ "n" }, "<leader>mx", ":!chmod +x %<CR><CR>", "chmod +x")
+map({ "n" }, "<leader>mc", "yygccp", "Duplicate and comment out line")
+map({ "x" }, "<leader>mr", ":<esc>:%s/\\%V[^A-Za-z0-9]/ /g<CR>", "Clean selection for type")
