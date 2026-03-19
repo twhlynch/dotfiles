454ecc6dd28abf41faeb02f98381c0c641a0bcdd
Tom Lynch <twhlynch@gmail.com>
2026-03-19 20:39:17 +1100

    add yankring autocommand

--- config/nvim/lua/twhlynch/autocmds.lua
+++ config/nvim/lua/twhlynch/autocmds.lua
@@ -27,3 +27,15 @@ autocmd({ "BufWinEnter", "WinEnter" }, {
 		end
 	end,
 })
+
+-- yankring
+autocmd("TextYankPost", {
+	group = augroup,
+	callback = function()
+		if vim.v.event.operator == "y" then
+			for i = 9, 1, -1 do -- shift all numbered registers
+				vim.fn.setreg(tostring(i), vim.fn.getreg(tostring(i - 1)))
+			end
+		end
+	end,
+})
