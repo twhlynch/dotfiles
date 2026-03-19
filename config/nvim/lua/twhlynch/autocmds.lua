87850541e53413bd08518bb8cd9042d854ff6d5e
Tom Lynch <twhlynch@gmail.com>
2026-03-19 20:38:56 +1100

    clean up autocommands file

--- config/nvim/lua/twhlynch/autocmds.lua
+++ config/nvim/lua/twhlynch/autocmds.lua
@@ -1,17 +1,26 @@
+local autocmd = vim.api.nvim_create_autocmd
+
+local augroup = vim.api.nvim_create_augroup("CustomAutoCmds", { clear = true })
+
 -- highlight when yanking text
-vim.api.nvim_create_autocmd({ "TextYankPost" }, {
-	desc = "Highlight when yanking (copying) text",
-	group = vim.api.nvim_create_augroup("HighlightOnYank", { clear = true }),
+autocmd({ "TextYankPost" }, {
+	group = augroup,
 	callback = function()
 		vim.hl.on_yank()
 	end,
 })
 
 -- no scrolloff if buffer readonly
-vim.api.nvim_create_autocmd({ "BufWinEnter", "WinEnter" }, {
-	group = vim.api.nvim_create_augroup("SetScrollOff", { clear = true }),
+autocmd({ "BufWinEnter", "WinEnter" }, {
+	group = augroup,
 	callback = function()
-		if vim.bo.buftype == "nowrite" or vim.bo.buftype == "prompt" or not vim.bo.modifiable or vim.bo.readonly then
+		local ignore_scrolloff = --
+			not vim.bo.modifiable -- not modifiable
+			or vim.bo.readonly -- readonly
+			or vim.bo.buftype == "nowrite" -- nowrite
+			or vim.bo.buftype == "prompt" -- prompt
+
+		if ignore_scrolloff then
 			vim.opt_local.scrolloff = 0
 		else
 			vim.opt_local.scrolloff = vim.g.user_scrolloff or 8
