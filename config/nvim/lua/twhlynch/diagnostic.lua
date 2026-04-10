dbcf2019b844683cc362017290d6e14415b486fe
Tom Lynch <twhlynch@gmail.com>
2026-04-11 02:41:43 +1000

    cooler diagnostics

--- config/nvim/lua/twhlynch/diagnostic.lua
+++ config/nvim/lua/twhlynch/diagnostic.lua
@@ -1,4 +1,19 @@
+local sev = vim.diagnostic.severity
+
 vim.diagnostic.config({
+	severity_sort = true,
+	update_in_insert = false,
 	virtual_text = true,
-	float = { border = "rounded" },
+	float = {
+		border = "rounded",
+		source = true,
+	},
+	signs = {
+		text = {
+			[sev.ERROR] = "󰅚",
+			[sev.WARN] = "󰀪",
+			[sev.INFO] = "",
+			[sev.HINT] = "󰌶",
+		},
+	},
 })
