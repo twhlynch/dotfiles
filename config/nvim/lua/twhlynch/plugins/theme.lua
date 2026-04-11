b194f4d33ad4838b9519ff156127d43088bbfa22
Tom Lynch <twhlynch@gmail.com>
2026-04-11 23:08:27 +1000

    dedupe theme colors

--- config/nvim/lua/twhlynch/plugins/theme.lua
+++ config/nvim/lua/twhlynch/plugins/theme.lua
@@ -1,38 +1,46 @@
 return {
 	"vague2k/vague.nvim",
 	config = function()
+		local c = {
+			bg = "#141415",
+			inactiveBg = "#1c1c24",
+			fg = "#cdcdcd",
+			floatBorder = "#878787",
+			line = "#252530",
+			comment = "#606079",
+			builtin = "#b4d4cf",
+			func = "#c48282",
+			string = "#e8b589",
+			number = "#e0a363",
+			property = "#c3c3d5",
+			constant = "#aeaed1",
+			parameter = "#bb9dbd",
+			visual = "#333738",
+			error = "#d8647e",
+			warning = "#f3be7c",
+			hint = "#7e98e8",
+			operator = "#90a0b5",
+			keyword = "#6e94b2",
+			type = "#9bb4bc",
+			search = "#405065",
+			plus = "#7fa563",
+			delta = "#f3be7c",
+
+			func_darker = "#bb7070",
+			keyword_darker = "#507796",
+			conflict = "#faa387",
+			red = "#ff0000",
+		}
+
 		for _, group in pairs(vim.fn.getcompletion("@lsp", "highlight")) do
 			vim.api.nvim_set_hl(0, group, {})
 		end
+
 		require("vague").setup({
 			transparent = true,
 			bold = true,
 			italic = true,
-			colors = {
-				bg = "#141415",
-				inactiveBg = "#1c1c24",
-				fg = "#cdcdcd",
-				floatBorder = "#878787",
-				line = "#252530",
-				comment = "#606079",
-				builtin = "#b4d4cf",
-				func = "#c48282",
-				string = "#e8b589",
-				number = "#e0a363",
-				property = "#c3c3d5",
-				constant = "#aeaed1",
-				parameter = "#bb9dbd",
-				visual = "#333738",
-				error = "#d8647e",
-				warning = "#f3be7c",
-				hint = "#7e98e8",
-				operator = "#90a0b5",
-				keyword = "#6e94b2",
-				type = "#9bb4bc",
-				search = "#405065",
-				plus = "#7fa563",
-				delta = "#f3be7c",
-			},
+			colors = c,
 			on_highlights = function(highlights, _)
 				-- no visual fg override
 				highlights.Visual.fg = nil
@@ -42,46 +50,46 @@ return {
 				-- no background on Pmenu
 				highlights.Pmenu.bg = nil
 				-- fix float border
-				highlights.FloatBorder = { fg = "#878787" }
+				highlights.FloatBorder = { fg = c.floatBorder }
 				-- blink cmp
-				highlights.BlinkCmpMenuSelection = { fg = "#AEAED1", bg = "#252530" } -- selected item
-				highlights.BlinkCmpLabel = { fg = "#cdcdcd" } -- item label
-				highlights.BlinkCmpLabelMatch = { fg = "#f3be7c" } -- matching text
+				highlights.BlinkCmpMenuSelection = { fg = c.constant, bg = c.line } -- selected item
+				highlights.BlinkCmpLabel = { fg = c.fg } -- item label
+				highlights.BlinkCmpLabelMatch = { fg = c.delta } -- matching text
 				highlights.BlinkCmpMenuBorder = { link = "FloatBorder" } -- border
-				highlights.BlinkCmpKind = { fg = "#AEAED1" } -- item icon
+				highlights.BlinkCmpKind = { fg = c.constant } -- item icon
 				-- errors
-				highlights.Error = { fg = "#d8647e" }
-				highlights.NvimInternalError = { fg = "#f3be7c", bg = "#ff0000" }
+				highlights.Error = { fg = c.error }
+				highlights.NvimInternalError = { fg = c.warning, bg = c.red }
 
 				-- oil git
-				highlights.OilGitModifiedStaged = { fg = "#f3be7c" }
-				highlights.OilGitModifiedUnstaged = { fg = "#e0a363" }
-				highlights.OilGitRenamed = { fg = "#7e98e8" }
-				highlights.OilGitDeleted = { fg = "#d8647e" }
-				highlights.OilGitCopied = { fg = "#7e98e8" }
-				highlights.OilGitConflict = { fg = "#faa387" }
-				highlights.OilGitUntracked = { fg = "#7fa563" }
-				highlights.OilGitAdded = { fg = "#7fa563" }
-				highlights.OilGitIgnored = { fg = "#606079" }
+				highlights.OilGitModifiedStaged = { fg = c.delta }
+				highlights.OilGitModifiedUnstaged = { fg = c.number }
+				highlights.OilGitRenamed = { fg = c.hint }
+				highlights.OilGitDeleted = { fg = c.error }
+				highlights.OilGitCopied = { fg = c.hint }
+				highlights.OilGitConflict = { fg = c.conflict }
+				highlights.OilGitUntracked = { fg = c.plus }
+				highlights.OilGitAdded = { fg = c.plus }
+				highlights.OilGitIgnored = { fg = c.comment }
 
 				-- darken readonly methods
-				highlights["@lsp.typemod.method.readonly.cpp"] = { fg = "#bb7070" }
+				highlights["@lsp.typemod.method.readonly.cpp"] = { fg = c.func_darker }
 				-- color functions
-				-- highlights["@lsp.typemod.function"].fg = "#c48282"
-				highlights["@lsp.type.function"].fg = "#c48282"
-				highlights["@function.call"].fg = "#c48282"
-				highlights["@function.method.call"].fg = "#c48282"
+				-- highlights["@lsp.typemod.function"].fg = c.func
+				highlights["@lsp.type.function"].fg = c.func
+				highlights["@function.call"].fg = c.func
+				highlights["@function.method.call"].fg = c.func
 				-- color macros like functions
-				highlights["@lsp.type.macro"].fg = "#c48282"
+				highlights["@lsp.type.macro"].fg = c.func
 				-- python docs are comments
-				highlights["@string.documentation.python"] = { fg = "#606079" }
+				highlights["@string.documentation.python"] = { fg = c.comment }
 				-- lua docs are darker
-				highlights["@keyword.return.luadoc"] = { fg = "#507796" }
-				highlights["@keyword.luadoc"] = { fg = "#507796" }
-				highlights["@lsp.typemod.keyword.documentation.lua"] = { fg = "#507796" }
+				highlights["@keyword.return.luadoc"] = { fg = c.keyword_darker }
+				highlights["@keyword.luadoc"] = { fg = c.keyword_darker }
+				highlights["@lsp.typemod.keyword.documentation.lua"] = { fg = c.keyword_darker }
 				-- lua macros are types
-				highlights["@lsp.type.macro.lua"] = { fg = "#9bb4bc" }
-				highlights["@function.macro.luadoc"] = { fg = "#9bb4bc" }
+				highlights["@lsp.type.macro.lua"] = { fg = c.type }
+				highlights["@function.macro.luadoc"] = { fg = c.type }
 			end,
 		})
 		vim.cmd("colorscheme vague")
