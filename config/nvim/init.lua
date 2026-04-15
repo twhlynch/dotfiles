5734a3e54953206796b5d743b5b7fa71611facca
Tom Lynch <twhlynch@gmail.com>
2026-04-15 17:59:40 +1000

    add profiling boilerplate

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -1,2 +1,14 @@
+if vim.env.PROF then
+	local snacks = vim.fn.stdpath("data") .. "/lazy/snacks.nvim"
+	vim.opt.rtp:append(snacks)
+	require("snacks.profiler").startup({
+		startup = {
+			event = "VimEnter",
+			-- event = "UIEnter",
+			-- event = "VeryLazy",
+		},
+	})
+end
+
 require("twhlynch")
 vim.lsp.log.set_level(vim.log.levels.ERROR)
