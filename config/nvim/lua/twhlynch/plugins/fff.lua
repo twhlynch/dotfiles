b3dab8ab59fff6b9d0346846a4432e52d09ab4f0
Tom Lynch <twhlynch@gmail.com>
2026-05-02 18:05:06 +1000

    make FFF look more like snacks

--- config/nvim/lua/twhlynch/plugins/fff.lua
+++ config/nvim/lua/twhlynch/plugins/fff.lua
@@ -1,7 +1,17 @@
+-- TODO: override fff picker to look and behave like snacks
 return {
 	"dmtrKovalenko/fff.nvim",
 	build = "cargo build --release",
-	opts = {},
+	opts = {
+		prompt = " ",
+		layout = {
+			width = 0.9,
+			prompt_position = "top",
+			flex = {
+				wrap = "bottom",
+			},
+		},
+	},
 	lazy = true,
 	keys = {
 		-- stylua: ignore start
