560e2fd4d62fbb6fb343b109a57278b59204104a
Tom Lynch <twhlynch@gmail.com>
2026-08-24 23:47:21 +1000

    commented jdtls config for when auto imports and annotations break

---
+++ config/nvim/lsp/jdtls.lua
@@ -0,0 +1,13 @@
+---@type vim.lsp.Config
+return {
+	-- init_options = {
+	-- 	settings = { java = { import = {
+	-- 		gradle = { enabled = false, annotationsProcessing = { enabled = false } },
+	-- 		maven = { enabled = false },
+	-- 	} } },
+	-- },
+	-- settings = { java = { import = {
+	-- 	gradle = { enabled = false, annotationsProcessing = { enabled = false } },
+	-- 	maven = { enabled = false },
+	-- } } },
+}
