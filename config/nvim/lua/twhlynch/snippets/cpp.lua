e9876315270e4acf000441a6637ad75acf6a9f7e
Tom Lynch <twhlynch@gmail.com>
2026-09-09 19:30:25 +1000

    remove rayne specific snippets

--- config/nvim/lua/twhlynch/snippets/cpp.lua
+++ config/nvim/lua/twhlynch/snippets/cpp.lua
@@ -31,38 +31,4 @@ return {
 			}
 		)
 	),
-	s("w", t("World::GetSharedInstance()")),
-	s("world", t("World::GetSharedInstance()")),
-	s("W", t("World* world = World::GetSharedInstance();")),
-	s("log", fmta("RNDebug(<>);", { i(1) })),
-	s("str", fmta('RNSTR("<>")', { i(1) })),
-	s("cstr", fmta('RNCSTR("<>")', { i(1) })),
-	s(
-		"traverse",
-		fmta(
-			[[
-	Traverse([&](SceneNode *node) {
-		<>
-	});
-	]],
-			{
-				i(1),
-			}
-		)
-	),
-	s(
-		"enumerate",
-		fmta(
-			[[
-	Enumerate<<<>>>([&](<> *element, size_t index, bool &stop) {
-		<>
-	});
-	]],
-			{
-				i(1, "Type"),
-				rep(1),
-				i(2),
-			}
-		)
-	),
 }
